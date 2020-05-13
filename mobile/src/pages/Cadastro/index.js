3ec5393a697f1152250bfd26b5087865dafceb60
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-05-13 00:20:20 -0300

    Cadastro Usuário App

--- mobile/src/pages/Cadastro/index.js
+++ mobile/src/pages/Cadastro/index.js
@@ -3,68 +3,88 @@ import React, { useState , useEffect, useRef } from 'react';
 import {
     SafeAreaView,
     KeyboardAvoidingView,
-    TouchableOpacity,
+    ImageBackground,
     Text
  } from 'react-native';
+
 import Loading from '../../components/Loading/index';
 
 import api from '../../services/index';
 import { Icon } from 'react-native-elements';
 
-import { Form } from '@unform/mobile';
-import { Scope } from '@unform/core';
-import * as Yup from 'yup';
-
-import Input from '../../components/Forms/Input/index';
 import {
-    Body,
-    ViewButton,
-    ViewForm
-} from './styles';
-export default function Cadastro({ navigation }){
+    Body,ViewButton,ViewForm,Br,Img,
+    Input,Button,TextButton,Label
 
-    const formRef = useRef(null);
+} from './styles';
+
+export default function Cadastro({ navigation }){
+    
+    const background = require('../../assets/img/bg-bus.jpg');
+    
+    //#region Declaração de Variavéis
+    
+    const [nome,setNome] = useState(null);
+    const [email,setEmail] = useState(null);
+    const [senha,setSenha] = useState(null);
+    const [confirm_senha,setConfirm] = useState(null);
+
+    const [loading,setLoading] = useState(false);
+    //#endregion 
+    
+    function register(){
+
+        setLoading(true);
+        const userData = {};
+        userData.nome = nome;
+        userData.email = email;
+        userData.senha = senha;
+        userData.confirm_senha = confirm_senha;
+        setLoading(false);
+        
+    }
     function goBack(){
         console.log('Oi');
         navigation.navigate('Login');
-    }
-    async function handleSubmit(){
-        console.log('COE');
     }
 
     useEffect(() => {
     }, []);
 
     return(
-        <SafeAreaView>
-            <KeyboardAvoidingView>
+       
+    <ImageBackground source={background}  style={{width: '100%', height: '100%'}} >  
+        <SafeAreaView>    
                 <Body>
-                    <ViewButton>
-                        <Icon  
-                            name='arrow-left'
-                            type='font-awesome'
-                            color='#48126a'
-                            size={32}
-                            onPress={goBack}
-                        />
-                    </ViewButton>
-                    <ViewForm>
-                        <Form ref={formRef} onSubmit={handleSubmit}>
-                            <Text>Oi</Text>
-                            <Input name="name"/>
-                            <Input type="email" name="email"/>
+                    <KeyboardAvoidingView  behavior="position" enabled>
+                        <ViewForm>
+                            <ViewButton>
+                                <Icon  
+                                    name='arrow-left'
+                                    type='font-awesome'
+                                    color='#48126a'
+                                    size={32}
+                                    onPress={goBack}
+                                />
+                            </ViewButton>
+                            
+                                <Img source={require('../../assets/img/amigo.png')}/>
+                                <Label>Bem vindo, vamos fazer seu cadastro!</Label>
+                                <Input value={nome} onChangeText={setNome} placeholder='Nome' placeholderTextColor='#424949'/>
+                                <Input value={email} onChangeText={setEmail} placeholder='E-mail' placeholderTextColor='#424949'/>
+                                <Input value={senha} onChangeText={setSenha} secureTextEntry placeholder='Senha' placeholderTextColor='#424949'/>
+                                <Input value={confirm_senha} onChangeText={setConfirm} secureTextEntry placeholder='Confirme sua senha' placeholderTextColor='#424949'/>            
+                                
+                                <Br/>
 
-                            <Scope path="address">
-                                <Input name="street" />
-                                <Input name="neighborhood"/>
-                                <Input name="city"/>
-                                <Input name="state"/>
-                                <Input name="number"/> 
-                            </Scope>
-                        </Form>
-                    </ViewForm>
+                                <Button onPress={register}>
+                                    <TextButton>Criar Conta</TextButton>
+                                </Button> 
+                        </ViewForm>
+                    </KeyboardAvoidingView>
                 </Body>
-            </KeyboardAvoidingView>
         </SafeAreaView>
+    </ImageBackground>
+        
     );
 }
--- mobile/src/pages/Cadastro/styles.js
+++ mobile/src/pages/Cadastro/styles.js
@@ -6,20 +6,79 @@ export const Body = styled.View`
     padding-left: 5px;
     padding-right: 5px;
     padding-bottom: 10px;
-    background-color: #f4f4f4;
     width: 100%;
     height: 100%;
 `;
 
 export const ViewButton = styled.View`
-    margin-top: 35px;
+    margin-top: 10px;
     margin-left: 10px;
     position: absolute;
 `;
 
 export const ViewForm = styled.View`
-    margin-top: 55px;
-    margin-left: 10px;
+    margin-top: 25px;
+    padding-left: 45px;
     position: relative;
+    background-color:#FCFCFC;
+    border-radius:10;
+`;
 
+
+export const Img = styled.Image`
+    height: 170px;
+    width: 170px;
+    margin-left:70px;
+    margin-top:90px;
+    margin-bottom: 10px;
+`;
+
+export const Input =styled.TextInput`
+    margin-top: 5px;
+    margin-bottom:10px;
+    padding-left: 0px;        
+    width: 300px;
+    height:40px;
+    border-color: #fff;
+    border-width: 2px;
+    border-radius: 10;
+    color: #000;
+    background-color:#FFF;
+    border-color: #444;
+    padding-left: 5px;
+`;
+
+export const Br = styled.View`
+    padding-top:5px;
+    padding-bottom: 5px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    position: relative;
+    width: 300px;
+    height: 40px;
+    background-color:#48126a;
+	border-radius:28px;
+	border:1px solid #ffffff;
+    color:#ffffff;
+    margin-bottom: 50px;
+
+`;
+
+export const TextButton = styled.Text`
+    color:#ffffff;
+    font-family:Roboto;
+    font-size:20px;
+    padding-top:4px;
+    margin-left:35%;
+    text-decoration:none;
+    text-shadow:0px 1px 0px #aad6a3;
+    font-weight: bold;
+`;
+
+export const Label = styled.Text`
+    font-family: Roboto;
+    color: #48126a;
+    font-size: 18px;
+    font-weight: bold;
 `;
